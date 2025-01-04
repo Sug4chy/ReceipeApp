22c7e3a1b4886ee51739c26d52714dd444bd2094
sug4chy <sb.9226981433@gmail.com>
2025-01-04 12:59:14 +0500

    Доверстал окно с аллергенами

--- app/src/main/java/ru/sug4chy/receipe_app/ui/main/MainFragment.kt
+++ app/src/main/java/ru/sug4chy/receipe_app/ui/main/MainFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.PopupWindow
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.sug4chy.receipe_app.R
 import ru.sug4chy.receipe_app.databinding.FragmentMainBinding
 import ru.sug4chy.receipe_app.databinding.PopupAllergensBinding
+import ru.sug4chy.receipe_app.ui.allergens.Allergen
+import ru.sug4chy.receipe_app.ui.allergens.AllergensListAdapter
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
@@ -18,6 +21,19 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val popupView = PopupAllergensBinding.inflate(layoutInflater, null, false)
+        popupView.allergensList.apply {
+            adapter = AllergensListAdapter().also {
+                it.submitList(
+                    listOf(
+                        Allergen(1, "potato"),
+                        Allergen(2, "orange"),
+                        Allergen(3, "apple"),
+                        Allergen(4, "choco"),
+                    )
+                )
+            }
+            layoutManager = LinearLayoutManager(requireContext())
+        }
         binding.root.post {
             PopupWindow(
                 popupView.root,
