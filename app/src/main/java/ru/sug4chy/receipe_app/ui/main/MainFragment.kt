990870d7dd8a36b311360b3c3b82b9ddf328d974
sug4chy <sb.9226981433@gmail.com>
2025-01-04 14:29:01 +0500

    Доделал UI слой для всплывающего окна, осталось лишь подключить БД

--- app/src/main/java/ru/sug4chy/receipe_app/ui/main/MainFragment.kt
+++ app/src/main/java/ru/sug4chy/receipe_app/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.PopupWindow
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.sug4chy.receipe_app.R
@@ -13,43 +14,45 @@ import ru.sug4chy.receipe_app.data.model.Allergen
 import ru.sug4chy.receipe_app.databinding.FragmentMainBinding
 import ru.sug4chy.receipe_app.databinding.PopupAllergensBinding
 import ru.sug4chy.receipe_app.ui.allergens.AllergensListAdapter
-import ru.sug4chy.receipe_app.utils.PopupAllergensBindingSetter
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     private val binding: FragmentMainBinding by viewBinding()
     private val viewModel: MainViewModel by viewModel()
-    private val popupBinding by lazy {
-        PopupAllergensBindingSetter.setUp(
-            binding = PopupAllergensBinding.inflate(layoutInflater, null, false),
-            adapter = allergensAdapter,
-            context = requireContext(),
-            onAddBtnPressed = ::onAddAllergenBtnClicked
-        )
-    }
+    private val popupBinding by lazy { setUpPopupAllergensBinding() }
 
-    private val allergensAdapter: AllergensListAdapter = AllergensListAdapter(::onDeleteAllergenBtnClicked)
+    private val allergensAdapter: AllergensListAdapter =
+        AllergensListAdapter(::onDeleteAllergenBtnClicked)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.allergens.observe(viewLifecycleOwner, ::onAllergensListed)
         viewModel.listAllergens()
+    }
 
-        binding.root.post {
-            PopupWindow(
-                popupBinding.root,
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                true
-            ).showAtLocation(view, Gravity.CENTER, 0, 0)
+    private fun setUpPopupAllergensBinding(): PopupAllergensBinding {
+        val binding = PopupAllergensBinding.inflate(layoutInflater, null, false)
+        binding.allergensList.apply {
+            this.adapter = adapter
+            this.layoutManager = LinearLayoutManager(context)
         }
+
+        binding.addAllergenBtn.setOnClickListener {
+            onAddAllergenBtnClicked(
+                popupBinding = binding
+            )
+        }
+
+        return binding
     }
 
     private fun onAllergensListed(allergens: List<Allergen>) =
         this@MainFragment.allergensAdapter.submitList(allergens)
 
-    private fun onAddAllergenBtnClicked() {
+    private fun onAddAllergenBtnClicked(
+        popupBinding: PopupAllergensBinding
+    ) {
         val enteredName = popupBinding.addAllergenTextField.text.toString()
         popupBinding.addAllergenTextField.text.clear()
 
@@ -58,4 +61,14 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     private fun onDeleteAllergenBtnClicked(id: Int) =
         viewModel.deleteAllergenById(id)
+
+    // Вызывать при нажатии на кнопку "Food allergens", когда нужно будет показать всплывающее окно
+    private fun showAllergensPopup() {
+        PopupWindow(
+            popupBinding.root,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            true
+        ).showAtLocation(view, Gravity.CENTER, 0, 0)
+    }
 }
