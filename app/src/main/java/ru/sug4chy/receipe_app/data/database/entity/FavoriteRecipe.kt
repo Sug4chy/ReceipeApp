e7b559ccfd2f6984d9ac144e1afa5a98c935593b
sug4chy <sb.9226981433@gmail.com>
2025-01-05 20:53:06 +0500

    Сверстал VH для избранного рецепта. Добавил размытие при появлении всплывающего окна с аллергенами

--- app/src/main/java/ru/sug4chy/receipe_app/data/database/entity/FavoriteRecipe.kt
+++ app/src/main/java/ru/sug4chy/receipe_app/data/database/entity/FavoriteRecipe.kt
@@ -1,9 +1,13 @@
 package ru.sug4chy.receipe_app.data.database.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class FavoriteRecipe(
-    @PrimaryKey(autoGenerate = true) val id: Int?
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo val name: String,
+    @ColumnInfo(name = "cooking_time") val cookingTime: String,
+    @ColumnInfo(name = "image_url") val imageURL: String
 )
--- app/src/main/java/ru/sug4chy/receipe_app/ui/favorites/FavoritesListAdapter.kt
+++ app/src/main/java/ru/sug4chy/receipe_app/ui/favorites/FavoritesListAdapter.kt
@@ -5,19 +5,28 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil3.load
 import ru.sug4chy.receipe_app.data.database.entity.FavoriteRecipe
 import ru.sug4chy.receipe_app.databinding.ViewholderFavoriteBinding
 
 class FavoritesListAdapter(
-
+    private val onIsFavoriteCheckboxUnselected: (Int) -> Unit
 ) : ListAdapter<FavoriteRecipe, FavoritesListAdapter.FavoriteViewHolder>(FavoriteDiffUtil()) {
 
-    class FavoriteViewHolder(
+    inner class FavoriteViewHolder(
         private val binding: ViewholderFavoriteBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(recipe: FavoriteRecipe) {
-
+            binding.recipeTitle.text = recipe.name
+            binding.cookingTime.text = recipe.cookingTime
+            binding.recipeImage.load(recipe.imageURL)
+            binding.isFavoriteCheckbox.isChecked = true
+            binding.isFavoriteCheckbox.setOnCheckedChangeListener { _, isChecked ->
+                if (!isChecked) {
+                    onIsFavoriteCheckboxUnselected(recipe.id!!)
+                }
+            }
         }
     }
 
